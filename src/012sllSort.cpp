e4fb035ca1b88f9beeb06ec5b5edbfffe618423b
MVSSRAJKIRAN <MVSSRAJKIRAN@missionrnd.com>
2016-03-17 08:33:47 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -12,7 +12,6 @@ ERROR CASES:
 
 NOTES: Only 0,1,2, will be in sll nodes
 */
-
 #include <stdio.h>
 #include <malloc.h>
 
@@ -22,5 +21,34 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	int *flag, size = 0;
+	flag = (int *)calloc(3, sizeof(int));
+	temp = head;
+	while (temp != NULL)
+	{
+		flag[temp->data]++;
+		temp = temp->next;
+		size++;
+	}
+	temp = head;
+	while (temp != NULL)
+	{
+		if (flag[0] > 0)
+		{
+			flag[0]--;
+			temp->data = 0;
+		}
+		else if (flag[1] > 0)
+		{
+			flag[1]--;
+			temp->data = 1;
+		}
+		else
+		{
+			flag[2]--;
+			temp->data = 2;
+		}
+		temp = temp->next;
+	}
 }
