c0d896efd2d18a689f74491da56df3733a20a8df
MVSSRAJKIRAN <MVSSRAJKIRAN@missionrnd.com>
2016-03-17 19:44:15 +0530

    Submit Commit

--- src/removeEveryKthNode.cpp
+++ src/removeEveryKthNode.cpp
@@ -10,14 +10,35 @@ ERROR CASES: Return NULL for error cases.
 
 NOTES:
 */
-
 #include <stdio.h>
-
 struct node {
 	int num;
 	struct node *next;
 };
-
 struct node * removeEveryKthNode(struct node *head, int K) {
-	return NULL;
+	struct node *temp;
+	int count = 1;
+	temp = head;
+	if (K == 1){
+		head = NULL;
+	}
+	else if (temp != NULL&&K > 0){
+		while (temp->next != NULL){
+			if ((count + 1) % K == 0){
+				temp->next = (temp->next)->next;
+				count = count + 2;
+			}
+			else{
+				count++;
+			}
+			if (temp->next != NULL)
+				temp = temp->next;
+			else
+				break;
+		}
+	}
+	else{
+		head = NULL;
+	}
+	return head;
 }
